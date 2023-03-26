44080033b3745516120c15b338987e4220f6cc98
vtyric <misha.torno@gmail.com>
2023-03-26 22:38:09 +0500

    упаковал приложение в докер. упаковал базу данных в докер. сделал докер для пг админа. сделал автомиграции при запуске приложения в докере.

--- back/HR-portal-api/Api/Program.cs
+++ back/HR-portal-api/Api/Program.cs
@@ -4,9 +4,14 @@ using Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 builder.Services.ConfigureSwaggerService();
-builder.Services.AddDalService();
+builder.Services.AddDalService(builder.Configuration.GetConnectionString("DB"));
 builder.Services.AddLogicServices();
 
 var app = builder.Build();
---
+++ back/HR-portal-api/Dal/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dal;
+
+public class Contract
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class DataContext : DbContext
+{
+    public DbSet<Contract> Contracts { get; set; }
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+}
