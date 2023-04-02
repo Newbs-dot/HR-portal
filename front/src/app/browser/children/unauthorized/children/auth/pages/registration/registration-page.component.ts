a6060f5863d2ff87740ce0e605cd40db9a3c5ff4
vtyric <misha.torno@gmail.com>
2023-04-02 23:27:32 +0300

    fix

--- front/src/app/browser/children/unauthorized/children/auth/pages/registration/registration-page.component.ts
+++ front/src/app/browser/children/unauthorized/children/auth/pages/registration/registration-page.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
     templateUrl: './registration-page.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    styleUrls: ['./registration-page.component.scss'],
+    styleUrls: ['./styles/registration-page.component.scss'],
 })
 export class RegistrationPageComponent {
+
     public readonly items: string[] = ['Option 1', 'Option 2', 'Option 3'];
 
     public testForm: FormGroup<{fio: FormControl<string | null>, login: FormControl<string | null>, password: FormControl<string | null>}> = new FormGroup({
@@ -13,6 +15,7 @@ export class RegistrationPageComponent {
         login: new FormControl('', Validators.required),
         password: new FormControl('', Validators.required),
     });
+
     public onClick(event: MouseEvent): void {
         console.info('click ', event);
     }
