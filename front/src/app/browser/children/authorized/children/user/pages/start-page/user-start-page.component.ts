7393a4b9fcf5eb07b968a0080c57afec3a56df80
lupriy <artemsved66@gmail.com>
2023-04-21 12:28:40 +0500

    added content user

--- front/src/app/browser/children/authorized/children/user/pages/start-page/user-start-page.component.ts
+++ front/src/app/browser/children/authorized/children/user/pages/start-page/user-start-page.component.ts
@@ -4,6 +4,7 @@ import { StartPageBaseComponent } from '../../../../../../../common';
 @Component({
     templateUrl: './user-start-page.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./styles/user-start-page.component.scss'],
 })
 export class UserStartPageComponent extends StartPageBaseComponent {
 }
