9c4f66bd809da63774fb01c89e39a0e8da06948e
Максим Мацулевич <uveoff@gmail.com>
2023-05-27 22:47:14 +0500

    bin-chilling

--- front/src/app/browser/children/authorized/children/user/pages/start-page/children/info/user-profile-info.component.ts
+++ front/src/app/browser/children/authorized/children/user/pages/start-page/children/info/user-profile-info.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     templateUrl: './user-profile-info.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./styles/user-profile-info.component.scss'],
 })
 export class UserProfileInfoComponent {
 
